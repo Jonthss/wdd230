784c3bcbf6b08b50c503f9552f0dad304ade4b73
Jonthss <jonathas9000@gmail.com>
2025-03-13 12:02:53 -0300

    W02: Team Activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,33 @@ document.getElementById('currentYear').textContent = new Date().getFullYear();
 
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+
+    if (hamButton.classList.contains('open')) {
+        hamButton.textContent = '✖'; // Close icon
+    } else {
+        hamButton.textContent = '☰'; // Menu icon
+    }
+});
+
+
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        themeToggle.textContent = '☀️ Light Mode';
+        localStorage.setItem('theme', 'dark');
+    } else {
+        themeToggle.textContent = '🌙 Dark Mode';
+        localStorage.setItem('theme', 'light');
+    }
+});
